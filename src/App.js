f316680a01a057086301d9143fff75f5a7e1ddef
LIAN007-ui <lianderclaret@gmail.com>
2026-01-30 18:40:15 -0500

    se elimino otro error

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const Login = React.lazy(() => import('./views/pages/login/Login'))
 const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
-const PlanPrimero = React.lazy(() => import('./views/plan/PlanPrimero'))
 
 import PrivateRoute from './components/PrivateRoute'
 
@@ -50,7 +49,6 @@ const App = () => {
         <Routes>
           <Route exact path="/" name="Login Page" element={<Login />} />
           <Route exact path="/login" name="Login Page" element={<Login />} />
-          <Route exact path="/plan" name="Plan Primer Semestre" element={<PlanPrimero />} />
           <Route exact path="/register" name="Register Page" element={<Register />} />
           <Route exact path="/404" name="Page 404" element={<Page404 />} />
           <Route exact path="/500" name="Page 500" element={<Page500 />} />
